19ad4b7769e592e80db8a9456047aef474119d97
endroid <info@endroid.nl>
2017-08-28 19:59:39 +0000

    Build frontend assets upon reset

--- reset.sh
+++ reset.sh
@@ -4,8 +4,9 @@ rm -rf var/cache/*
 
 php-cs-fixer fix src
 
-composer install
 yarn
+composer install
+node_modules/.bin/encore production
 
 bin/console doctrine:database:drop --force
 bin/console doctrine:database:create
