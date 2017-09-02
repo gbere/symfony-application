f867b85df5059bda4208c44d606d9338a9c73640
endroid <info@endroid.nl>
2017-09-02 13:46:21 +0000

    Use Bootstrap 4 beta

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,3 +1,6 @@
+global.Popper = require('popper.js/dist/umd/popper');
+
 require('./../../node_modules/bootstrap/dist/css/bootstrap.css');
+require('./../../node_modules/bootstrap/dist/js/bootstrap.js');
 require('./../../node_modules/font-awesome/css/font-awesome.css');
 require('./../css/base.css');
