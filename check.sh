0d2cea3a67e043c9d7412aaff68cd107aceb3f91
endroid <info@endroid.nl>
2017-09-02 13:45:56 +0000

    Add CS fixer and security checker to check script

--- check.sh
+++ check.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-vendor/bin/simple-phpunit
+php-cs-fixer fix src
+security-checker security:check
 vendor/bin/behat
+vendor/bin/simple-phpunit
 node_modules/.bin/psi endroid.nl --threshold=75
