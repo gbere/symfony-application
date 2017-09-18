3c5d668456711ae85395d0ab0f759ae70a477bb9
endroid <info@endroid.nl>
2017-09-18 22:55:51 +0200

    Add tile and data sanitize new style

--- reset.sh
+++ reset.sh
@@ -12,11 +12,9 @@ bin/console doctrine:schema:update --force
 
 bin/console doctrine:fixtures:load -n --fixtures=src/DataFixtures/ORM
 
-# Data Sanitize
-bin/console endroid:data-sanitize:load-example-data
-
-# Import
+# Import / data sanitize demo commands
 bin/console endroid:import-demo:generate-data
+bin/console endroid:data-sanitize-demo:generate-data
 
 # Metrics
 vendor/bin/phpmetrics --report-html=public/metrics .
